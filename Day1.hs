4be18f35109c760e10bf13fa85a59b7b3876a845
Marta Gómez Macías <marta@wazuh.com>
2019-12-01 23:56:09 +0100

    Day 1, part 2

--- Day1.hs
+++ Day1.hs
@@ -1,7 +1,15 @@
 import System.IO
+import Data.List
+
+single_fuel :: (Fractional a, RealFrac a) => a -> Int
+single_fuel mass
+    | mass <= 0 = 0
+    | otherwise = (floor $ mass / 3) - 2
 
 total_fuel :: (Fractional a, RealFrac a) => [a] -> Int
-total_fuel input = sum $ map (\x -> (floor (x / 3)) - 2) input
+total_fuel input = floor $ sum $ map additional_fuel input
+    where
+        additional_fuel m = sum $ tail $ unfoldr (\x -> if x <= 0 then Nothing else Just (x, fromIntegral $ single_fuel x)) m
 
 main :: IO()
 main = do
