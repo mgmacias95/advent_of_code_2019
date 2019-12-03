1f8745dfd4b50ed772e21b691d4c9edb1aba1eaf
Marta Gómez Macías <marta@wazuh.com>
2019-12-03 21:25:45 +0100

    Day 2, part 2

--- Day2.hs
+++ Day2.hs
@@ -2,13 +2,13 @@ import System.IO
 import Data.List.Split
 import Data.List
 
-restore_program :: [Int] -> [Int]
-restore_program program = head program : 12 : 2 : drop 3 program
+replace_noun_verb :: [Int] -> (Int, Int) -> [Int]
+replace_noun_verb program (noun, verb) = head program : noun : verb : drop 3 program
 
-run_program :: [Int] -> Int -> [Int]
-run_program program position
+run_program :: Int -> [Int] -> [Int]
+run_program position program
     | head ptail == 99 = program
-    | otherwise = run_program (fhead ++ final_val:tail ftail) (position + 4)
+    | otherwise = run_program (position + 4) (fhead ++ final_val:tail ftail)
     where
         (phead, ptail) = splitAt position program
         op = if head ptail == 1 then (+) else (*)
@@ -18,10 +18,12 @@ run_program program position
         final_val = op val1 val2
         (fhead, ftail) = splitAt fpos program
 
+find_combination :: [Int] -> (Int, Int)
+find_combination program = head $ dropWhile (\(x, y) -> head (run_program 0 $ replace_noun_verb program (x, y)) /= 19690720) (concat $ map (\y -> map (\x -> (y, x)) [0..99]) [0..99])
+
 main :: IO()
 main = do
     contents <-  readFile "data/day2.txt"
     let program = map read $ splitOn "," contents :: [Int]
-    let restored_program = restore_program program
-    let final = run_program restored_program 0
-    putStr $ (show $ head final) ++ "\n"
+    let (noun, verb) = find_combination program
+    putStr $ show (100 * noun + verb) ++ "\n"
