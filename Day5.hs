cc6be4790604144cc4e88c8c237fb9c068cec690
Marta Gómez Macías <marta@wazuh.com>
2019-12-07 19:50:29 +0100

    Day 5, part 2

--- Day5.hs
+++ Day5.hs
@@ -1,5 +1,4 @@
 import System.IO
-import System.IO.Unsafe
 import Data.List.Split
 import Data.List
 import Data.Char
@@ -23,16 +22,20 @@ get_input = do
     return io_int_input
 
 
-run_program :: Int -> [Int] -> [Int]
-run_program position program = case (d*10 + e) of 99 -> program
-                                                  1  -> run_program (position + 4) (update_list program (v1 + v2) i3)
-                                                  2  -> run_program (position + 4) (update_list program (v1 * v2) i3)
-                                                  3  -> run_program (position + 2) (update_list program (unsafePerformIO get_input) i1)
-                                                  4  -> run_program (position + 2) (program ++ [v1])
+run_program :: Int -> Int -> [Int] -> [Int]
+run_program input position program = case (d*10 + e) of 99 -> program
+                                                        1  -> run_program input (position + 4) (update_list program (v1 + v2) i3)
+                                                        2  -> run_program input (position + 4) (update_list program (v1 * v2) i3)
+                                                        3  -> run_program input (position + 2) (update_list program input i1)
+                                                        4  -> run_program input (position + 2) (program ++ [v1])
+                                                        5  -> run_program input (if v1 == 0 then (position + 3) else v2) program
+                                                        6  -> run_program input (if v1 /= 0 then (position + 3) else v2) program
+                                                        7  -> run_program input (position + 4) (update_list program (if v1 < v2 then 1 else 0) i3)
+                                                        8  -> run_program input (position + 4) (update_list program (if v1 == v2 then 1 else 0) i3)
     where
         (_, ptail) = splitAt position program
         a:b:c:d:e:[] = zfill 5 $ list_digits $ head ptail
-        _:i1:[] = take 2 ptail
+        i0:i1:[] = take 2 ptail
         _:_:i2:i3:[] = take 4 ptail
         v1 = if c == 0 then (program !! i1) else i1
         v2 = if b == 0 then (program !! i2) else i2
@@ -40,7 +43,9 @@ run_program position program = case (d*10 + e) of 99 -> program
 
 main :: IO()
 main = do
-    contents <-  readFile "data/day5.txt"
+    contents <- readFile "data/day5.txt"
     let program = map read $ splitOn "," contents :: [Int]
-    let result = run_program 0 program
+    input <- getLine
+    let int_input = read input :: Int
+    let result = run_program int_input 0 program
     putStr $ (show $ last result) ++ "\n"
